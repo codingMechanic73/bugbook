7f6ec9b8a16e9327eb2d345f944538d73cc3800c
codingMechanic73 <alanwinston2019@gmail.com>
2024-08-16 20:56:58 +0530

    add playwright

--- tests/e2e/smoke.spec.ts
+++ tests/e2e/smoke.spec.ts
@@ -3,9 +3,8 @@ import { test, expect } from "@playwright/test";
 test("should allow you to login and logout", async ({ page }) => {
   const email = "t@example.com";
   const password = "12121212";
-  const url = "http://localhost:5173/";
   const name = "test";
-  await page.goto(url);
+  await page.goto("/");
   await page.getByRole("link", { name: "Sign In" }).click();
   await page.getByPlaceholder("Email").fill(email);
   await page.getByPlaceholder("Password").fill(password);
